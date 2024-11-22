3f2587d31b11efb2b3ebeea038343cff878db527
Vivienne Ansah <voa2000@hotmail.com>
2024-11-22 16:14:47 +0000

    amended: files for argocd

---
+++ app-2/webapp.py
@@ -0,0 +1,8 @@
+from flask import request, Flask
+import json
+webapp = Flask(__name__)
+@awebapp.route('/')
+def hello_world():
+  return 'ArgoCD course, this is Webapp :) '
+if __name__ == '__main__':
+  webapp.run(debug=True, host='0.0.0.0')
